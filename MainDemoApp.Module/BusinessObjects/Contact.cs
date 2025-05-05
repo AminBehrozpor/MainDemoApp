d27a30adec872551ee5ff3ed679bd34b9b1fa930
AminBehrozpor <Aminbehrozpor1@gmail.com>
2025-05-05 13:28:47 +0330

    Add an Action with Option Selection

--- MainDemoApp.Module/BusinessObjects/Contact.cs
+++ MainDemoApp.Module/BusinessObjects/Contact.cs
@@ -167,8 +167,11 @@ namespace MainDemoApp.Module
     }
     public enum Priority
     {
+        [ImageName("State_Priority_Low")]
         Low = 0,
+        [ImageName("State_Priority_Normal")]
         Normal = 1,
+        [ImageName("State_Priority_High")]
         High = 2
     }
 }
